f56ed7233cb101c45ff4a6c1ef38653619896d9f
minhyuk <minhyuki89@gmail.com>
2021-07-03 13:54:27 +0900

    initial

---
+++ src/main/java/com/glm/test/gleamedia/GleamediaApplication.java
@@ -0,0 +1,13 @@
+package com.glm.test.gleamedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GleamediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GleamediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glm/test/gleamedia/GleamediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glm.test.gleamedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GleamediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
