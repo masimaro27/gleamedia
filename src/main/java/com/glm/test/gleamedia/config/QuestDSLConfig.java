76ddf66e22afeff41c6a6407a11bbf77edefa229
minhyuk <minhyuki89@gmail.com>
2021-07-07 18:17:01 +0900

    QueryDSL config 추가

---
+++ src/main/java/com/glm/test/gleamedia/config/QuestDSLConfig.java
@@ -0,0 +1,21 @@
+package com.glm.test.gleamedia.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuestDSLConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
