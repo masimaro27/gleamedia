263de489ad0a4e58f68c13d9bf88d6b1f0f44f5b
minhyuk <minhyuki89@gmail.com>
2021-07-07 18:21:49 +0900

    CORS config 추가

---
+++ src/main/java/com/glm/test/gleamedia/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.glm.test.gleamedia.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+}
